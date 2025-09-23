8e804e79f3af4308672538898c86d2509e81b3d9
shengyu <15022273097@163.com>
2025-09-23 10:14:28 +0800

    配置路径别名

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //导入provider组件
 import { RouterProvider } from 'react-router-dom'
-import router from './router/index.tsx'
+import router from '@/router/index'
 
 createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router}>
