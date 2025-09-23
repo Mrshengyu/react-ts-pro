5dc0b619ce1bfb21e91997554c6a2984c888a7ce
shengyu <15022273097@163.com>
2025-09-23 23:25:28 +0800

    home模块，list无限加载

--- src/pages/Home/HomeList/index.tsx
+++ src/pages/Home/HomeList/index.tsx
@@ -1,10 +1,11 @@
-import { Image, List } from 'antd-mobile';
+import { Image, List, InfiniteScroll } from 'antd-mobile';
 // mock数据
 import { users } from './user'
 import React, { useState, useEffect } from 'react';
+import { fetchListAPI } from '@/apis/list';
 // import { ListRes } from '@/apis/list'
 
-type Props={
+type Props = {
     channelId: string,
 }
 
@@ -19,7 +20,7 @@ const HomeList = (props: Props) => {
     useEffect(() => {
         const getList = async () => {
             try {
-                const res = await ListRes({
+                const res = await fetchListAPI({
                     channel_id: '0',
                     timestamp: "" + new Date().getTime()
                 })
@@ -29,12 +30,48 @@ const HomeList = (props: Props) => {
                     pre_timetamp: res.data.data.pre_timestamp
                 })
 
-            } catch (err) { }
-            throw new Error('获取列表数据失败')
+            } catch (err) {
+                throw new Error('获取列表数据失败')
+            }
         }
         // getList()
         setListRes(users)
     }, [channelId])
+
+    // 开关 标记当前是否有新数据
+    // 触发条件 1 hasMore 2 loadMore
+    // true 代表有新数据 可以继续触发 loadMore
+    // false 代表没有新数据 不能继续触发 loadMore   
+    const [hasMore, setHasMore] = useState(true)
+
+    // 加载更多
+    const loadMore = async () => {
+        // 这里模拟接口请求
+        console.log('上拉加载触发')
+        try {
+            const res = await fetchListAPI({
+                channel_id: channelId,
+                timestamp: listRes.pre_timetamp
+            })
+            // 合并数据
+            setListRes({
+                results: [...listRes.results, ...res.data.results],
+                pre_timetamp: res.data.data.pre_timestamp
+            })
+
+        } catch (err) {
+            throw new Error('获取列表数据失败')
+        }
+
+        // 这里模拟判断是否还有更多数据
+        if (res.data.data.results.length === 0) {
+            setHasMore(false)
+
+        }
+
+    }
+
+
     return (
         <>
             <List>
@@ -53,6 +90,7 @@ const HomeList = (props: Props) => {
                     </List.Item>
                 ))}
             </List>
+            <InfiniteScroll loadMore={loadMore} hasMore={hasMore} threshold={100} />
         </>
     )
 }
